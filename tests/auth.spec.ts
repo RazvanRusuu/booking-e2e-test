94cbec86c969a9e520ed551fdd7a8886d0aede7f
Razvan Rusu <razvanrusu9420@gmail.com>
2024-01-31 05:49:40 +0200

    init tests

---
+++ tests/auth.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+
+const UI_URL = "http://localhost:5173/";
+
+test("should allow the user to sign in", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByRole("link", { name: "Sign in" }).click();
+
+  await expect(page.getByRole("heading", { name: "Sign in" })).toBeVisible();
+
+  await page.locator("[name=email]").fill("razvan@email.com");
+  await page.locator("[name=password]").fill("pass1234");
+
+  await page.getByRole("button", { name: "Log in" }).click();
+
+  await expect(page.getByText("Successfully logged in")).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
+});
