7a4b9d850e0a6f58b6f3f9e61f89a78263f61ade
Razvan Rusu <razvanrusu9420@gmail.com>
2024-02-08 06:23:03 +0200

    init tests

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -7,15 +7,28 @@ test("should allow the user to sign in", async ({ page }) => {
 
   await page.getByRole("link", { name: "Sign in" }).click();
 
-  await expect(page.getByRole("heading", { name: "Sign in" })).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
 
   await page.locator("[name=email]").fill("razvan@email.com");
   await page.locator("[name=password]").fill("pass1234");
 
-  await page.getByRole("button", { name: "Log in" }).click();
+  await page.getByRole("button", { name: "Sign In" }).click();
 
-  await expect(page.getByText("Successfully logged in")).toBeVisible();
-  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
-  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
-  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
+  await expect(page.getByText("You are signed in")).toBeVisible();
+  await expect(page.getByRole("link", { name: "Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "Hotels" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
 });
+
+test("should allow user to logout", async ({ page }) => {
+  await page.goto(`${UI_URL}sign-in`);
+
+  await page.locator("[name=email]").fill("razvan@email.com");
+  await page.locator("[name=password]").fill("pass1234");
+  await page.getByRole("button", { name: "Sign In" }).click();
+
+  await page.getByRole("button", { name: "Logout" }).click();
+  await expect(page.getByText("You have been logged out")).toBeVisible();
+});
+
+// todo test create an account
